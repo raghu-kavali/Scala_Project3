aa8608306d451faa5316f196e13b61ba14fffaa4
srikarkona <60153561+srikarkona@users.noreply.github.com>
2020-03-15 23:26:26 -0400

    Create join.scala

---
+++ join.scala
@@ -0,0 +1,26 @@
+package mcit.scala.project
+
+trait Join[L, R, Q] {
+  def join(a: List[L], b: List[R]): List[Q]
+}
+
+case class JoinOutput(left: Any, right: Option[Any])
+
+class GenericNestedLoopJoin extends Join[RouteTrip, Calendar, EnrichedTrip] {
+  override def join(a: List[RouteTrip], b: List[Calendar]): List[EnrichedTrip] =
+    for {
+    routeTrip <- a
+    calendar <- b
+    if routeTrip.trip.service_id == calendar.service_id
+  }
+
+   yield EnrichedTrip(routeTrip, Some(calendar))
+}
+class MapJoin extends Join[Trip, Route, RouteTrip] {
+
+  override def join(a: List[Trip], b: List[Route]): List[RouteTrip] = {
+    val t: Map[String, Route] = b.map(route => route.route_id -> route).toMap
+    a.filter(trip => t.contains(trip.route_id)).map(trip => RouteTrip(trip, Some(t(trip.route_id))))
+  }
+
+}
